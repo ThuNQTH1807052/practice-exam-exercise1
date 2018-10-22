31d202c0726da7c7fd3fb2d3fddc6ad87cbd51fd
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-22 18:58:44 +0700

    exercise1

---
+++ cmake-build-debug/CMakeFiles/exercise1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise1.dir/main.c.o"
+  "exercise1.pdb"
+  "exercise1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exercise1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
